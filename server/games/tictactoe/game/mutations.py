bccc2bba83edd9fa5b1ca3ae3180d32b968952cf
vijay.krishna <vijay.krishna@quantilope.com>
2022-05-11 19:34:30 +0200

    Set up join game

--- server/games/tictactoe/game/mutations.py
+++ server/games/tictactoe/game/mutations.py
@@ -1,28 +1,47 @@
 import logging
 import graphene
 from tictactoe.models import Player
-from tictactoe.player.player import PlayerType
 from tictactoe.models import Game
-
+from tictactoe.game.subscriptions import JoinGameSubscription
 from tictactoe.game.game import GameType
 
 
 class CreateGame(graphene.Mutation):
-  class Arguments:
-      player_1 = graphene.ID(required=True)
-  
-  game = graphene.Field(lambda: GameType)
-  
-  def mutate(root, info, player_1):
-    game = Game()
-    player = Player.objects.get(pk=player_1)
-    game = Game(
-      id=None,
-      player_1=player,
-    )
-    game.players_count = 1
-    game.save()
-    return CreateGame(game=game)
+    class Arguments:
+        player_1 = graphene.ID(required=True)
+
+    game = graphene.Field(lambda: GameType)
+
+    def mutate(root, info, player_1):
+        game = Game()
+        player = Player.objects.get(pk=player_1)
+        game = Game(
+            id=None,
+            player_1=player,
+        )
+        game.players_count = 1
+        game.save()
+        return CreateGame(game=game)
+
+
+class JoinGameMutation(graphene.Mutation):
+    class Arguments:
+        game_id = graphene.ID(required=True)
+        player_id = graphene.ID(required=True)
+
+    game = graphene.Field(lambda: GameType)
+
+    def mutate(root, info, game_id, player_id):
+        game = Game.objects.get(pk=game_id)
+        player = Player.objects.get(pk=player_id)
+        game.player_2 = player
+        game.players_count = game.players_count + 1
+        game.status = Game.GameStatus.ACTIVE
+        game.save()
+        JoinGameSubscription.on_join_game(game)
+        return JoinGameMutation(game=game)
+
 
 class GameMutation(graphene.ObjectType):
-  create_game = CreateGame.Field()
+    create_game = CreateGame.Field()
+    join_game = JoinGameMutation.Field()
--- server/games/tictactoe/game/subscriptions.py
+++ server/games/tictactoe/game/subscriptions.py
@@ -1,15 +1,14 @@
 from tokenize import group
 import channels_graphql_ws
 import graphene
+from tictactoe.game.game import GameType
 
-from tictactoe.models import Game, Player
 
-
-class JoinGame(channels_graphql_ws.Subscription):
+class JoinGameSubscription(channels_graphql_ws.Subscription):
     class Arguments:
         game_id = graphene.ID(required=True)
 
-    event = graphene.String()
+    event = graphene.Field(lambda: GameType)
 
     @staticmethod
     def subscribe(self, info, game_id):
@@ -18,13 +17,13 @@ class JoinGame(channels_graphql_ws.Subscription):
         return ['game_' + str(game_id)]
 
     @staticmethod
-    def publish(payload, info):
-        return JoinGame(event=payload)
+    def publish(payload, info, game_id=None):
+        return JoinGameSubscription(event=payload)
 
     @classmethod
-    def on_join_game(cls, game_id, player_id):
-        return cls.broadcast(group='game_'+str(game_id), payload={"event": "join_game", "game_id": game_id, "player_id": player_id})
+    def on_join_game(cls, game):
+        return cls.broadcast(group='game_'+str(game.id), payload=game)
 
 
 class GameSubscriptions(graphene.ObjectType):
-    on_join_game = JoinGame.Field()
+    on_join_game = JoinGameSubscription.Field()
